b042c7c398a07bdef94851fcfef38c49658427a0
szhao <szhao@indeed.com>
2022-05-20 22:01:48 +0900

    return return variable

--- solver/optimal_solver.py
+++ solver/optimal_solver.py
@@ -2,12 +2,13 @@ import random
 
 import pyomo.environ as pyo
 from pyomo.core import maximize
+from pyomo.kernel import pprint
 
 
 # %%
 def solve(ctrs: list[float], costs: list[float], budget: float):
     model = pyo.ConcreteModel()
-    model.x = pyo.Var([i for i in range(len(ctrs))], domain=pyo.NonNegativeReals)
+    model.x = pyo.Var(pyo.RangeSet(0, len(ctrs) - 1), domain=pyo.NonNegativeReals, bounds=(0, 1))
     model.y = pyo.Objective(
         expr=sum([ctrs[i] * model.x[i] for i in range(len(ctrs))]),
         sense=maximize
@@ -16,8 +17,10 @@ def solve(ctrs: list[float], costs: list[float], budget: float):
         expr=sum([costs[i] * model.x[i] for i in range(len(ctrs))]) <= budget)
     opt = pyo.SolverFactory('glpk')
     opt.solve(model)
-    return model.y()
+    model.x.display()
+    model.y.display()
+    return model.y(), [model.x[i].value for i in model.x]
 
 
 # %%
-# solve([random.random() for i in range(500)], [random.random() for i in range(500)], 100)
+solve([random.random() for i in range(500)], [random.random() for i in range(500)], 100)
