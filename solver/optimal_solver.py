aac06a4127cab508258649e52c99fb3dfec95ffb
szhao <szhao@indeed.com>
2022-05-21 16:52:31 +0900

    raw highs solver is slow

--- solver/optimal_solver.py
+++ solver/optimal_solver.py
@@ -2,26 +2,42 @@ import random
 import timeit
 
 import pyomo.environ as pyo
+from numpy.core.records import ndarray
 from pyomo.core import maximize
 from pyomo.kernel import pprint
+import numpy as np
+from scipy import optimize
+
+from enum import Enum
+
+
+class Solver(Enum):
+    GLPK = 1
+    HIGHS = 2
 
 
 # %%
-def solve(ctrs: list[float], costs: list[float], budget: float):
-    model = pyo.ConcreteModel()
-    model.x = pyo.Var(pyo.RangeSet(0, len(ctrs) - 1), domain=pyo.NonNegativeReals, bounds=(0, 1))
-    model.y = pyo.Objective(
-        expr=sum([ctrs[i] * model.x[i] for i in range(len(ctrs))]),
-        sense=maximize
-    )
-    model.Constraint1 = pyo.Constraint(
-        expr=sum([costs[i] * model.x[i] for i in range(len(ctrs))]) <= budget)
-    opt = pyo.SolverFactory('glpk')
-    opt.solve(model)
-    return model.y()
-    # return model.y(), [model.x[i].value for i in model.x]
+def solve(ctrs: ndarray, costs: ndarray, budget: float, solver: Solver = Solver.HIGHS):
+    if solver == Solver.GLPK:
+        model = pyo.ConcreteModel()
+        model.x = pyo.Var(pyo.RangeSet(0, len(ctrs) - 1), domain=pyo.NonNegativeReals, bounds=(0, 1))
+        model.y = pyo.Objective(
+            expr=sum([ctrs[i] * model.x[i] for i in range(len(ctrs))]),
+            sense=maximize
+        )
+        model.Constraint1 = pyo.Constraint(
+            expr=sum([costs[i] * model.x[i] for i in range(len(ctrs))]) <= budget)
+        opt = pyo.SolverFactory('glpk')
+        opt.solve(model)
+        return model.y()
+        # return model.y(), [model.x[i].value for i in model.x]
+    else:
+        bounds = [(0,1.0) for i in range(len(ctrs))]
+        min = optimize.linprog(ctrs * -1, A_ub=np.expand_dims(costs, axis=0), b_ub=np.array([budget], ndmin=2), bounds=bounds, method='highs')
+        print((min.x > 0.0).sum())
+        return -min.fun
 
 
 # %%
 # variables = 500 * 96
-# %timeit solve([random.random() for i in range(variables)], [random.random() for i in range(variables)], 100)
+# %timeit solve(np.array([random.random() for i in range(variables)]), np.array([random.random() for i in range(variables)]), 100, solver = Solver.GLPK)
